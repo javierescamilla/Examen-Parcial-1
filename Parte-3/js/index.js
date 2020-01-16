58e96b362c8e57590dbbfa22f6452569139fb3c5
javierescamilla <javier_escamillaj@hotmail.com>
2020-01-16 08:26:56 -0600

    Tercera parte funcionando sin CSS

---
+++ Parte-3/js/index.js
@@ -0,0 +1,25 @@
+let nameUser = document.getElementsByClassName("userName");
+let commentUser = document.getElementById("userComment");
+let addCommentButton =  document.getElementById("submit");
+let commentSection =  document.getElementsByClassName("seccionComments");
+
+addCommentButton.addEventListener("click", function(event){
+    event.preventDefault();
+    if (nameUser[0].value === ''){
+        alert("No es posible registrar un nombre vacio");
+    }
+    if (commentUser.value === ''){
+        alert("No es posible registrar un comentario vacio");
+    }
+    else{
+        let boxNameUser = document.createTextNode(nameUser[0].value)
+        let commentNameUser = document.createTextNode(commentUser.value)
+        let space = document.createElement("br");
+        commentSection[0].appendChild(boxNameUser);
+        commentSection[0].appendChild(commentNameUser);
+        commentSection[0].appendChild(space);
+    }
+    document.getElementsByClassName("userName") = "";
+    document.getElementById("userComment") = "";
+});
+ 
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
+fork
 git clone https://github.com/javierescamilla/Examen-Parcial-1.git
 git add .
 git commit -m "Accesibilidad"
 git push
-git pull request
+pull request
+
